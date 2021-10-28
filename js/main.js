4ec5e990963299d30dc7951fb404a8d26515426a
IwaLain <arthes.menetil@gmail.com>
2021-10-28 17:28:13 +0300

    added validation

--- js/main.js
+++ js/main.js
@@ -10,7 +10,8 @@ const alert = document.querySelector('.alert')
 
 let users = []
 let userId = 1
-
+let validationErrors = []
+let validationIsBad = false
 
 openModal.addEventListener('click', (e) => {
     modal.classList.toggle('visible')
@@ -68,6 +69,8 @@ const ajaxPatch = (url, data, callback) => {
 }
 
 const showAlert = (type) => {
+    alert.textContent = ''
+
     switch (type) {
         case 'Success':
             alert.classList.add('alert-success')
@@ -76,11 +79,51 @@ const showAlert = (type) => {
         case 'Error':
             alert.classList.add('alert-danger')
             alert.textContent = 'Ошибка!'
+            break
+        case 'ValidationError':
+            alert.classList.add('alert-danger')
+            for (let err of validationErrors) {
+                alert.textContent += err + ' '
+            }
+
     }
     alert.classList.toggle('visible')
     setTimeout(() => {
         alert.classList = 'alert'
     }, 1500)
+}
+
+const validation = (el) => {
+    const name = /^[a-zA-Z]+$/
+    const phone = /^[0-9\-\+]{9,15}$/
+
+    switch(el.id) {
+        case 'firstName':
+            if (!name.test(el.value)) {
+                validationErrors[0] = 'Name is not valid.'
+            } else if (el.value.length <= 3) {
+                validationErrors[0] = 'Name must contain more than 3 symbols.'
+            } else {
+                validationErrors[0] = ''
+            }
+            break
+        case 'lastName':
+            if (!name.test(el.value)) {
+                validationErrors[1] = 'Name is not valid.'
+            } else if (el.value.length <= 3) {
+                validationErrors[1] = 'Name must contain more than 3 symbols.'
+            } else {
+                validationErrors[1] = ''
+            }
+            break
+        case 'phone':
+            if (!phone.test(el.value)) {
+                validationErrors[2] = 'Phone is not valid.'
+            } else {
+                validationErrors[2] = ''
+            }
+            break
+    }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -97,21 +140,37 @@ document.addEventListener('DOMContentLoaded', () => {
 form.addEventListener('submit', (e) => {
     e.preventDefault()
 
-    let user = {
-        id: userId,
-        firstName: firstName.value,
-        lastName: lastName.value,
-        email: email.value,
-        phone: phone.value
+    for (let el of form.elements) {
+        if (el.tagName === 'INPUT') {
+            validation(el)
+        }
     }
 
-    const newUsers = users.map((el) => {
-        return el.id === userId ? user : el
-    })
+    validationIsBad = false
 
-    modal.classList.remove('visible')
+    for (let err of validationErrors) {
+        if (err) validationIsBad = true
+    }
 
-    ajaxPatch('db.json', JSON.stringify(newUsers), (res) => {
-        showAlert(res)
-    })
+    if (!validationIsBad) {
+        let user = {
+            id: userId,
+            firstName: firstName.value,
+            lastName: lastName.value,
+            email: email.value,
+            phone: phone.value
+        }
+    
+        const newUsers = users.map((el) => {
+            return el.id === userId ? user : el
+        })
+    
+        modal.classList.remove('visible')
+    
+        ajaxPatch('db.json', JSON.stringify(newUsers), (res) => {
+            showAlert(res)
+        })
+    } else {
+        showAlert('ValidationError')
+    }
 })
